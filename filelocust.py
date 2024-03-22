10099a49b40e06017d0bbf4c87a9ce096aed0153
3sara <84321374+3sara@users.noreply.github.com>
2024-03-22 14:27:38 +0100

    Update filelocust.py

--- filelocust.py
+++ filelocust.py
@@ -62,36 +62,8 @@ class CustomUser(HttpUser):
                 f.write(f"Exception during DELETE request: {e} for user {self.username}.\n")
 
 
-
-
-#    @task
-#    def upload_file(self):
-#        file_name = "deletable.txt"
-        #with open(file_name, 'rb') as f:
-        #    response = self.client.put("/remote.php/dav/files/" + self.username + "/" + file_name,
-        #            auth=self.credentials, data=f, name="/remote.php/dav/files/[user]/deletable.txt")
-
-        #if response.status_code not in [201, 204]:
-        #    with open("output_modified.txt", "a") as f:
-        #        f.write(f"Error during PUT request: {response.status_code} for user {self.username}.\n")
-        #    return
-
-        #for i in range(0, 5):
-        #    self.client.get("/remote.php/dav/files/" + self.username + "/" + file_name,
-        #                    auth=self.credentials, name="/remote.php/dav/files/[user]/deletable.txt")
-
-        #response_delete=self.client.delete("/remote.php/dav/files/" + self.username + "/" + file_name,
-        #                   auth=self.credentials, name="/remote.php/dav/files/[user]/deletable.txt")
-#
-#
-#        if response_delete.status_code not in [201, 204]:
-#            with open("output_modified.txt", "a") as f:
-#                write(f"Error during DELETE request: {response_delete.status_code} for user {self.username}.\n")
-#            return
-
     @task
-    def additional_task(self):
-        # Example of an additional task
+    def get_task(self):
         response = self.client.get("/remote.php/dav/files/" + self.username + "/", auth=self.credentials)
         if response.status_code != 200:
             with open("output_modified.txt", "a") as f:
